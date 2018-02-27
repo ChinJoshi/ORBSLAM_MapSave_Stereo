2e16fa9ca2f0c41fa76e5869c67f26884af7bb97
bingo <892844406@qq.com>
2018-02-27 08:30:36 +0800

    first commit

---
+++ Thirdparty/g2o/build/CMakeFiles/g2o.dir/DependInfo.cmake
@@ -0,0 +1,61 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/stuff/os_specific.c" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/stuff/os_specific.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/core/batch_stats.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/batch_stats.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/core/cache.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/cache.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/core/estimate_propagator.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/estimate_propagator.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/core/factory.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/factory.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/core/hyper_dijkstra.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/hyper_dijkstra.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/core/hyper_graph.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/hyper_graph.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/core/hyper_graph_action.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/hyper_graph_action.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/core/jacobian_workspace.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/jacobian_workspace.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/core/marginal_covariance_cholesky.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/marginal_covariance_cholesky.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/core/matrix_structure.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/matrix_structure.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/core/optimizable_graph.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/optimizable_graph.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/core/optimization_algorithm.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/optimization_algorithm.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/core/optimization_algorithm_factory.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/optimization_algorithm_factory.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/core/optimization_algorithm_levenberg.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/optimization_algorithm_levenberg.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/core/optimization_algorithm_with_hessian.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/optimization_algorithm_with_hessian.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/core/parameter.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/parameter.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/core/parameter_container.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/parameter_container.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/core/robust_kernel.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/robust_kernel.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/core/robust_kernel_factory.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/robust_kernel_factory.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/core/robust_kernel_impl.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/robust_kernel_impl.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/core/solver.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/solver.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/core/sparse_optimizer.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/sparse_optimizer.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/stuff/property.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/stuff/property.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/stuff/string_tools.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/stuff/string_tools.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/stuff/timeutil.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/stuff/timeutil.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/types/types_sba.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/types/types_sba.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/types/types_seven_dof_expmap.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/types/types_seven_dof_expmap.cpp.o"
+  "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/g2o/types/types_six_dof_expmap.cpp" "/home/bingo/ORBSLAM_MapSave/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/types/types_six_dof_expmap.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "UNIX"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../core"
+  "../types"
+  "../stuff"
+  "/usr/include/eigen3"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
